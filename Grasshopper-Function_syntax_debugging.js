c573f155ba706508b3fee617e5d0372c7b717d42
David Iradukunda <iradukuda10@gmail.com>
2022-01-13 22:34:21 -0600

    Grasshopper-Function_syntax_debugging
    Grasshopper - Function syntax debugging
    A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.

---
+++ Grasshopper-Function_syntax_debugging.js
@@ -0,0 +1,10 @@
+/*
+Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+*/
+
+//answer    
+//switched brackets around parameters from '[]' to '()' and and a '{' after them
+function main(verb, noun) {
+    return verb + noun
+}
