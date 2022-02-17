ecc3a3632abb85108428809d4351faf7c1f9d87b
David Iradukunda <iradukuda10@gmail.com>
2022-02-17 15:50:53 -0600

    Exclamation_marks_series_#6-Remove_n_exclamation_marks_in_the_sentence_from_left_to_right
    Description:
    Remove n exclamation marks in the sentence from left to right. n is positive integer.
    
    Examples
    remove("Hi!",1) === "Hi"
    remove("Hi!",100) === "Hi"
    remove("Hi!!!",1) === "Hi!!"
    remove("Hi!!!",100) === "Hi"
    remove("!Hi",1) === "Hi"
    remove("!Hi!",1) === "Hi!"
    remove("!Hi!",100) === "Hi"
    remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
    remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
    remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
    remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"

---
+++ Exclamation_marks_series_#6-Remove_n_exclamation_marks_in_the_sentence_from_left_to_right.js
@@ -0,0 +1,31 @@
+/*
+Description:
+Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+Examples
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+*/
+
+//answer    
+function remove(s, n) {
+    //return the s with the exclamation marks removed used a reduced method 
+    return s.split('').reduce((finalString, char) => {
+        let exclToRem = n
+        if (char == '!' && n) {
+            n--
+            return finalString
+        } else {
+            return finalString + char
+        }
+    }, '')
+}
