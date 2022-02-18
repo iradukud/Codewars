ab79cb3cedc343157cce2b2228ebd456acef722f
David Iradukunda <iradukuda10@gmail.com>
2022-02-18 15:51:15 -0600

    Enumerable_Magic_#3-Does_My_List_Include_This
    Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.

---
+++ Enumerable_Magic_#3-Does_My_List_Include_This.js
@@ -0,0 +1,9 @@
+/*
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+
+//answer    
+function include(arr, item) {
+    //return a boolean value based on if arr has item
+    return arr.includes(item)
+}
