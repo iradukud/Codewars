dd9ce0b7a887dfd86d62883d4a90257581d727d1
David Iradukunda <iradukuda10@gmail.com>
2022-03-11 19:33:54 -0600

    Power
    The goal is to create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times).
    
    Examples
    numberToPower(3,2)  // -> 9 ( = 3 * 3 )
    numberToPower(2,3)  // -> 8 ( = 2 * 2 * 2 )
    numberToPower(10,6) // -> 1000000
    Note: Math.pow and some other Math functions like eval() and ** are disabled.

---
+++ Power.js
@@ -0,0 +1,19 @@
+/*
+The goal is to create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times).
+
+Examples
+numberToPower(3,2)  // -> 9 ( = 3 * 3 )
+numberToPower(2,3)  // -> 8 ( = 2 * 2 * 2 )
+numberToPower(10,6) // -> 1000000
+Note: Math.pow and some other Math functions like eval() and ** are disabled.
+*/
+
+//answer    
+function numberToPower(number, power) {
+    //return number to the power of power using loops
+    let powered = 1
+    for (i = 1; i <= power; i++) {
+        powered *= number
+    }
+    return powered
+}
