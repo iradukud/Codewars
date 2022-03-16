46fd56bf4a7122437d9425d00d9672c3b270cae2
David Iradukunda <iradukuda10@gmail.com>
2022-03-16 10:40:59 -0500

    Convert a number into a string value
    We need a function that can transform a number into a string.

---
+++ Convert_a_Number_to_a_String.js
@@ -0,0 +1,15 @@
+/*
+    We need a function that can transform a number into a string.
+
+    What ways of achieving this do you know?
+
+    Examples:
+    123 --> "123"
+    999 --> "999"
+*/
+
+//answer
+function numberToString(num) {
+    //Converts number to string via built in methods
+    return String(num)
+  }
