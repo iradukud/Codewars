06441ed9c5c848117d4a52774157503b4cafcbd0
David Iradukunda <iradukuda10@gmail.com>
2022-03-16 10:29:22 -0500

    Convert Boolean value to Yes or No
    Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.

---
+++ Convert_boolean_values_to_strings_Yes_or_No.js
@@ -0,0 +1,14 @@
+/*
+    Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+//answer
+function boolToWord( bool ){
+    //if the boolean value is true return a yes string
+    if(bool){
+      return 'Yes'
+    // else return a No string
+    }else{
+     return 'No' 
+    }
+  }
