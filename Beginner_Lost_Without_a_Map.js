37021a20240ce95e87c35b3491d74a9b2abae38e
David Iradukunda <iradukuda10@gmail.com>
2022-03-20 13:52:13 -0500

    Beginner_Lost_Without_a_Map
    Given an array of integers, return a new array with each value doubled.

---
+++ Beginner_Lost_Without_a_Map.js
@@ -0,0 +1,13 @@
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+//answer
+function maps(x) {
+    //Returns a array x back with every element doubled
+    return x.map(i => i * 2)
+}
