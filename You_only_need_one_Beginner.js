7759a8d384ad95fedf8a9360238759a83e1ebcb6
David Iradukunda <iradukuda10@gmail.com>
2022-03-20 13:51:04 -0500

    You_only_need_one_Beginner
    +You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
    
    Array can contain numbers or strings. X can be either.
    
    Return true if the array contains the value, false if not.

---
+++ You_only_need_one_Beginner.js
@@ -0,0 +1,13 @@
+/*
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+*/
+
+//answer
+function check(a, x) {
+    // Use a method to check if the array-a contains value x and returns a true or false boolen value
+    return a.includes(x)
+}
