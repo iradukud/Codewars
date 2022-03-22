b78a1d59d7e9a7c97dd12d2170164a72c8392ed4
David Iradukunda <iradukuda10@gmail.com>
2022-03-22 13:07:30 -0500

    Convert_a_Boolean_to_a_String
    Implement a function which convert the given boolean value into its string representation.

---
+++ Convert_a_Boolean_to_a_String.js
@@ -0,0 +1,7 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+*/
+
+//answer
+//converts boolean to string using String() method
+const booleanToString = b => String(b)
