d178444925b8e951ca88d5f0055055f66d4b92be
David Iradukunda <iradukuda10@gmail.com>
2022-03-23 11:16:35 -0500

    MakeUpperCase
    Write a function which converts the input string to uppercase.

---
+++ MakeUpperCase.js
@@ -0,0 +1,8 @@
+/*
+Write a function which converts the input string to uppercase.
+*/
+
+//answer
+//
+//Use toUpperCase method to convert the whole string uppercases
+const makeUpperCase = str => str.toUpperCase()
