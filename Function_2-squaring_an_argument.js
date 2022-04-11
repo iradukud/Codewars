a8d478de8b62817358da9aec1b5aaee07819c03a
David Iradukunda <iradukuda10@gmail.com>
2022-04-11 17:20:02 -0500

    Function_2-squaring_an_argument
    Now you have to write a function that takes an argument and returns the square of it.

---
+++ Function_2-squaring_an_argument.js
@@ -0,0 +1,7 @@
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+//answer    
+//simple function that takes in a number and returns it's square value
+const square = num => Math.pow(num,2)
