c5848736319d093148ae799db0914d27562fe40a
David Iradukunda <iradukuda10@gmail.com>
2022-04-15 11:52:15 -0500

    Define_a_card_suit
    You get any card as an argument. Your task is to return the suit of this card (in lowercase).

---
+++ Define_a_card_suit.js
@@ -0,0 +1,18 @@
+/*
+You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+        '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+        '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+        '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+*/
+
+//answer    
+//compare the last string character to at suite hex value and return their string name 
+const defineSuit = card => card[card.length - 1]=='\u2663'? 'clubs' : card[card.length - 1]=='\u2666'? 'diamonds' : card[card.length - 1]=='\u2665'?'hearts' :'spades'
