2a6dc1f4e22cf6e8f80a350e3f3c9cb1011f3423
David Iradukunda <iradukuda10@gmail.com>
2022-04-16 15:09:37 -0500

    Pirates!!_Are_the_Cannons_ready!
    Ahoy Matey!
    Welcome to the seven seas.
    You are the captain of a pirate ship.
    You are in battle against the royal navy.
    You have cannons at the ready.... or are they?
    Your task is to check if the gunners are loaded and ready, if they are: Fire!
    If they aren't ready: Shiver me timbers!
    Your gunners for each test case are 2, 3 or 4.
    When you check if they are ready their answers are in a dictionary and will either be: aye or nay
    Firing with less than all gunners ready is non-optimum (this is not fire at will, this is fire by the captain's orders or walk the plank, dirty sea-dog!)
    If all answers are 'aye' then Fire! if one or more are 'nay' then Shiver me timbers!

---
+++ Pirates!!_Are_the_Cannons_ready!.js
@@ -0,0 +1,36 @@
+/*
+Ahoy Matey!
+
+Welcome to the seven seas.
+
+You are the captain of a pirate ship.
+
+You are in battle against the royal navy.
+
+You have cannons at the ready.... or are they?
+
+Your task is to check if the gunners are loaded and ready, if they are: Fire!
+
+If they aren't ready: Shiver me timbers!
+
+Your gunners for each test case are 2, 3 or 4.
+
+When you check if they are ready their answers are in a dictionary and will either be: aye or nay
+
+Firing with less than all gunners ready is non-optimum (this is not fire at will, this is fire by the captain's orders or walk the plank, dirty sea-dog!)
+
+If all answers are 'aye' then Fire! if one or more are 'nay' then Shiver me timbers!
+
+Also, check out the new Pirates!! Kata: https://www.codewars.com/kata/57e2d5f473aa6a476b0000fe
+*/
+
+//answer    
+const cannonsReady = (gunners) => {
+    if (gunners.Mike == 'nay' || gunners.Joe == 'nay' || gunners.Johnson == 'nay' || gunners.Peter == 'nay') {
+        //if any of the properties have a nay string value return 'Shiver me timbers!' string message  
+        return 'Shiver me timbers!'
+    } else {
+        //else return the 'Fire!' string message 
+        return 'Fire!'
+    }
+}
