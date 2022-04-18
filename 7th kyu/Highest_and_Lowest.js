457ae2efb93ab9b0afa21f104f0453f8ef0fefb8
David Iradukunda <iradukuda10@gmail.com>
2022-04-18 09:28:00 -0500

    Highest_and_Lowest
    In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.

---
+++ 7th kyu/Highest_and_Lowest.js
@@ -0,0 +1,17 @@
+/*
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Examples
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+Notes
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.
+*/
+
+//answer    
+//for each math method, the string list of numbers is split into a array then place via spread operator
+//into a math function then concatenate them
+const highAndLow=numbers => Math.max(...numbers.split(' '))+' '+Math.min(...numbers.split(' '))
