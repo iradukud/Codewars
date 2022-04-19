2e1bbbab85b9a4fe995466e89c4473b44b96ab4a
David Iradukunda <iradukuda10@gmail.com>
2022-04-19 05:06:47 -0500

    Categorize_New_Member
    The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
    
    To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
    
    Input
    Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
    
    Output
    Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.

---
+++ 7th kyu/Categorize_New_Member.js
@@ -0,0 +1,22 @@
+/*
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input
+Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Output
+Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+Example
+input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+*/
+
+//answer    
+//since every element will always have an array with 2 elements
+//so I used a map to reduce the 2 elements to one
+//to-do so checked if specific condition both elements and return a corresponding
+//string depending on the condition of both elements
+const openOrSenior = data => data.map(x=> x[0]>=55 && x[1]>7? 'Senior':'Open')
