9951003a6eebbd0cf1c6a4eb890a8219e561c8c3
David Iradukunda <iradukuda10@gmail.com>
2022-04-23 12:49:36 -0500

    Bin_to_Decimal
    Complete the function which converts a binary number (given as a string) to a decimal number.

---
+++ Bin_to_Decimal.js
@@ -0,0 +1,7 @@
+/*
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
+
+//answer    
+//converts a biinary string number to a decimal number
+const binToDec = bin => parseInt(bin,2)
