4acfbe692a1684890cd4fc6938b41284c51c7020
David Iradukunda <iradukuda10@gmail.com>
2022-04-24 06:44:22 -0500

    Grasshopper-Variable_Assignment_Debug
    Variable assignment
    Fix the variables assigments so that this code stores the string 'devLab' in the variable name.

---
+++ Grasshopper-Variable_Assignment_Debug.js
@@ -0,0 +1,11 @@
+/*
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+*/
+
+//answer    
+//simply the double equal signs
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
