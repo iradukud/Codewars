3c5dffeda8023cebb0f73f936e8f664d43fc24cd
David Iradukunda <iradukuda10@gmail.com>
2022-04-25 08:52:54 -0500

    Two_to_One
    Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.

---
+++ 7th kyu/Two_to_One.js
@@ -0,0 +1,17 @@
+/*
+Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+*/
+
+//answer    
+//combine both the strings and created an array with unique characters from both
+//sorted the the unique characters
+//then joined them and return the string of unique elements
+const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
