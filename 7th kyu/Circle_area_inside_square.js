c03c165a91e940c74a153673e9c4bb7b617a46c5
David Iradukunda <iradukuda10@gmail.com>
2022-04-26 12:16:02 -0500

    Circle_area_inside_square
    Your function gets a number which represents the area of the square and should return the area of the circle. The tests are rounded by 8 decimals to make sure multiple types of solutions work.

---
+++ 7th kyu/Circle_area_inside_square.js
@@ -0,0 +1,18 @@
+/*
+Circle area inside square
+Turn an area of a square in to an area of a circle that fits perfectly inside the square.
+
+inscribed circle
+
+You get the blue+red area and need to calculate the red area.
+
+Your function gets a number which represents the area of the square and should return the area of the circle. The tests are rounded by 8 decimals to make sure multiple types of solutions work.
+
+You don't have to worry about error handling or negative number input: area >= 0.
+
+This kata might be simpler than you expect, but good luck!
+*/
+
+//answer    
+//area of circle is a squared, so to get the radius squared divide the area by 4
+const squareAreaToCircle = size => size/4*Math.PI
