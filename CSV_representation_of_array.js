d2b98bc08d97bec354ef4439935a39e22b6b1f42
David Iradukunda <iradukuda10@gmail.com>
2022-05-05 11:11:31 -0500

    CSV_representation_of_array
    Create a function that returns the CSV representation of a two-dimensional numeric array.
    
    Example:
    
    input:
       [[ 0, 1, 2, 3, 4 ],
        [ 10,11,12,13,14 ],
        [ 20,21,22,23,24 ],
        [ 30,31,32,33,34 ]]
    
    output:
         '0,1,2,3,4\n'
        +'10,11,12,13,14\n'
        +'20,21,22,23,24\n'
        +'30,31,32,33,34'
    Array's length > 2.

---
+++ CSV_representation_of_array.js
@@ -0,0 +1,24 @@
+/*
+Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+Example:
+
+input:
+   [[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]] 
+    
+output:
+     '0,1,2,3,4\n'
+    +'10,11,12,13,14\n'
+    +'20,21,22,23,24\n'
+    +'30,31,32,33,34'
+Array's length > 2.
+*/
+
+//answer    
+const toCsvText = function (array) {
+    //joins all the array via a '\n' string and creates one string with all the arrays  
+    return array.join('\n')
+}
