52ee22e01ec7e49c479f5f733eefb1c0b7bd75e0
David Iradukunda <iradukuda10@gmail.com>
2022-05-07 16:35:16 -0500

    Push_a_hash-an_object_into_array
    You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!

---
+++ Push_a_hash-an_object_into_array.js
@@ -0,0 +1,8 @@
+/*
+You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+*/
+
+//answer    
+items = []
+//did not have the right syntax originally (did'nt us brackets around the items pushed)
+items.push({ a: "b", c: "d" })
