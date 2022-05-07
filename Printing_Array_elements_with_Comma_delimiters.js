5613519135024eed9c21b058de76c59f3e8ae644
David Iradukunda <iradukuda10@gmail.com>
2022-05-07 16:35:39 -0500

    Printing_Array_elements_with_Comma_delimiters
    Input: Array of elements
    
    ["h","o","l","a"]
    
    Output: String with comma delimited elements of the array in th same order.
    
    "h,o,l,a"

---
+++ Printing_Array_elements_with_Comma_delimiters.js
@@ -0,0 +1,14 @@
+/*
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+*/
+
+//answer    
+function printArray(array) {
+    return array.join(',')
+}
