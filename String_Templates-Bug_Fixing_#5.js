e00ae557ee9b5b9d01787588391cd08aba4efc60
David Iradukunda <iradukuda10@gmail.com>
2022-05-09 13:06:45 -0500

    String_Templates-Bug_Fixing_#5
    Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!

---
+++ String_Templates-Bug_Fixing_#5.js
@@ -0,0 +1,9 @@
+/*
+Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+*/
+
+//answer    
+function buildString(...template) {
+    //changed some syntax errors
+    return `I like ${template.join(', ')}!`;
+}
