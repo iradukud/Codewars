9cbaf4b50b30bbf5fcbc9a80d50271b89545f699
David Iradukunda <iradukuda10@gmail.com>
2022-05-16 10:57:30 -0500

    Type_of_sum
    Return the type of the sum of the two arguments

---
+++ Type_of_sum.js
@@ -0,0 +1,9 @@
+/*
+Return the type of the sum of the two arguments
+*/
+
+//answer    
+function typeOfSum(a, b) {
+    //return the type of value the sum of a and b produces
+    return typeof (a + b)
+}
