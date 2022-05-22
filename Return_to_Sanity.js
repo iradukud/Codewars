5b3a45ebc02524e083022ea6953c4220968704e0
David Iradukunda <iradukuda10@gmail.com>
2022-05-22 13:39:03 -0500

    Return_to_Sanity
    This function should return an object, but it's not doing what's intended. What's wrong?

---
+++ Return_to_Sanity.js
@@ -0,0 +1,11 @@
+/*
+This function should return an object, but it's not doing what's intended. What's wrong?
+*/
+
+//answer    
+function mystery() {
+    var results =
+        { sanity: 'Hello' }
+    //brought result inline to return   
+    return results
+}
