71faf6061c14e9e5234efa6e6308bacb018d6e1d
David Iradukunda <iradukuda10@gmail.com>
2022-05-23 11:24:45 -0500

    Training_JS_#21-Methods_of_String_object--trim()_and_the_string_template
    Task
    Coding in function fiveLine, function accept 1 parameter:s. s is a string.
    
    Please return a string of 5 lines(newline symbol is \n). The first line has one s; Second line have two s; and so on..Fifth line have five s;
    
    Note1: The two sides of the parameter s may contain some whitespace, please clear them before using s.
    
    Note2: Using a string template can make your job easier.
    
    Example:
    
    fiveLine("  a") should return "a\naa\naaa\naaaa\naaaaa"
    a
    aa
    aaa
    aaaa
    aaaaa       <---The effect when you console.log it
    fiveLine("  xy ")
    should return "xy\nxyxy\nxyxyxy\nxyxyxyxy\nxyxyxyxyxy"
    xy
    xyxy
    xyxyxy
    xyxyxyxy
    xyxyxyxyxy  <---The effect when you console.log it

---
+++ Training_JS_#21-Methods_of_String_object--trim()_and_the_string_template.js
@@ -0,0 +1,34 @@
+/*
+Task
+Coding in function fiveLine, function accept 1 parameter:s. s is a string.
+
+Please return a string of 5 lines(newline symbol is \n). The first line has one s; Second line have two s; and so on..Fifth line have five s;
+
+Note1: The two sides of the parameter s may contain some whitespace, please clear them before using s.
+
+Note2: Using a string template can make your job easier.
+
+Example:
+
+fiveLine("  a") should return "a\naa\naaa\naaaa\naaaaa"
+a
+aa
+aaa
+aaaa
+aaaaa       <---The effect when you console.log it
+fiveLine("  xy ") 
+should return "xy\nxyxy\nxyxyxy\nxyxyxyxy\nxyxyxyxyxy"
+xy
+xyxy
+xyxyxy
+xyxyxyxy
+xyxyxyxyxy  <---The effect when you console.log it
+*/
+
+//answer    
+function fiveLine(s) {
+    //variable that holds the modified string, removed all the extra white space
+    const stringz = s.trim()
+    //return the string in the requested format using template literals
+    return `${stringz}\n${stringz.repeat(2)}\n${stringz.repeat(3)}\n${stringz.repeat(4)}\n${stringz.repeat(5)}`
+}
