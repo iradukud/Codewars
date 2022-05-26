f168e0445f6f939e7d6a8272ead63eff208f969d
David Iradukunda <iradukuda10@gmail.com>
2022-05-26 10:46:42 -0500

    Mr_Freeze
    There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.

---
+++ Mr_Freeze.js
@@ -0,0 +1,7 @@
+/*
+There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+*/
+
+//answer    
+//used method to prevent any changes from being made to an object
+Object.freeze(MrFreeze)
