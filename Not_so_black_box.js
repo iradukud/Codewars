46998cbf7acb43710df9ca6627be6018b3b72c89
David Iradukunda <iradukuda10@gmail.com>
2022-05-29 10:59:18 -0500

    Not_so_black_box
    The kata has been preloaded with a function called blackBox. Figure out what the function does.
    
    You may need to think laterally!

---
+++ Not_so_black_box.js
@@ -0,0 +1,12 @@
+/*
+The kata has been preloaded with a function called blackBox. Figure out what the function does.
+
+You may need to think laterally!
+*/
+
+//answer    
+blackBox()
+//converted the function to a string to see it's contents 
+console.log(blackBox+'')
+//then call the necessary function to pass the kata
+JSopenSesame()
