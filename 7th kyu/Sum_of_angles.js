6e9a1d97808e4bf4b42e480a8f976858615b7005
David Iradukunda <iradukuda10@gmail.com>
2022-06-02 10:41:23 -0500

    Sum_of_angles
    Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.

---
+++ 7th kyu/Sum_of_angles.js
@@ -0,0 +1,9 @@
+/*
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+*/
+
+//answer    
+function angle(n) {
+    //inputed n in a formula and return the result
+    return (n - 2) * 180
+}
