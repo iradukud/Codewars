50c3319d3423383af311904d685792daed39d72a
David Iradukunda <iradukuda10@gmail.com>
2022-06-06 11:25:57 -0500

    Greet_Me
    Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
    
    Example:
    
    "riley" --> "Hello Riley!"
    "JACK"  --> "Hello Jack!"

---
+++ 7th kyu/Greet_Me.js
@@ -0,0 +1,14 @@
+/*
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+*/
+
+//answer    
+var greet = function (name) {
+    //return the message as a string with modifications done to the name to standardize its presentation
+    return 'Hello ' + name[0].toUpperCase() + name.slice(1).toLowerCase() + '!'
+}
