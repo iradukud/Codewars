36fd72b76b3c9fb12e57edb459734648daab91d1
David Iradukunda <iradukuda10@gmail.com>
2022-06-07 10:43:50 -0500

    Sum_of_Triangular_Numbers
    Your task is to return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
    
    Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.) obtained by continued summation of the natural numbers 1, 2, 3, 4, 5, etc."
    
    [01]
    02 [03]
    04 05 [06]
    07 08 09 [10]
    11 12 13 14 [15]
    16 17 18 19 20 [21]
    e.g. If 4 is given: 1 + 3 + 6 + 10 = 20.
    
    Triangular Numbers cannot be negative so return 0 if a negative number is given.

---
+++ 7th kyu/Sum_of_Triangular_Numbers.js
@@ -0,0 +1,23 @@
+/*
+Your task is to return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
+
+Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.) obtained by continued summation of the natural numbers 1, 2, 3, 4, 5, etc."
+
+[01]
+02 [03]
+04 05 [06]
+07 08 09 [10]
+11 12 13 14 [15]
+16 17 18 19 20 [21]
+e.g. If 4 is given: 1 + 3 + 6 + 10 = 20.
+
+Triangular Numbers cannot be negative so return 0 if a negative number is given.
+*/
+
+//answer    
+function sumTriangularNumbers(n) {
+    //if n is less then 0 return 0
+    if (n < 0) { return 0 }
+    //else if plug n into the formula and return the sum of triangular numbers
+    return (n * (n + 1) * (n + 2)) / 6
+}
