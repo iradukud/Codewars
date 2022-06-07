7cb8469bc027dc17aafda141d09e895109055a98
David Iradukunda <iradukuda10@gmail.com>
2022-06-07 10:44:19 -0500

    Sort_arrays-1
    Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.

---
+++ 7th kyu/Sort_arrays-1.js
@@ -0,0 +1,11 @@
+/*
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+*/
+
+//answer    
+// input: names - unsorted array
+// output: sorted array
+sortme = function (names) {
+    //return the list of names sorted in lexicographical order
+    return names.sort()
+}
