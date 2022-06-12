6d9a896b0d85ce477000793565809b99de053dd2
David Iradukunda <iradukuda10@gmail.com>
2022-06-12 15:59:44 -0500

    Parts_of_a_list
    Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
    
    Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
    Each part will be in a string
    Elements of a pair must be in the same order as in the original array.
    Examples of returns in different languages:
    a = ["az", "toto", "picaro", "zone", "kiwi"] -->
    [["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]]
    or
     a = {"az", "toto", "picaro", "zone", "kiwi"} -->
    {{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
    or
    a = ["az", "toto", "picaro", "zone", "kiwi"] -->
    [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
    or
    a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
    [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
    or
    a = ["az", "toto", "picaro", "zone", "kiwi"] -->
    "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
    Note
    You can see other examples for each language in "Your test cases"

---
+++ 7th kyu/Parts_of_a_list.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION:
+Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+
+Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+Each part will be in a string
+Elements of a pair must be in the same order as in the original array.
+Examples of returns in different languages:
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+[["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
+or
+ a = {"az", "toto", "picaro", "zone", "kiwi"} -->
+{{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
+or
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+[("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+or 
+a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
+[("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+or
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+"(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
+Note
+You can see other examples for each language in "Your test cases"
+*/
+
+//Solution    
+function partlist(arr) {
+    //for each element to two parts from the original array with i element(s) joined (by ' ') to the left and elements left of i joined (by ' ')
+    //then return the modified array with the first element removed because it' has an empty element
+    return arr.map((v, i) => [arr.slice(0, i).join(' '), arr.slice(i).join(' ')]).slice(1)
+}
