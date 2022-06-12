7334183d0018dd5d8c2703dd716d14c2fe537571
David Iradukunda <iradukuda10@gmail.com>
2022-06-12 15:59:58 -0500

    Number_Of_Occurrences
    Write a function that returns the number of occurrences of an element in an array.
    
    Examples
    var arr = [0, 1, 2, 2, 3];
    arr.numberOfOccurrences(0) === 1;
    arr.numberOfOccurrences(4) === 0;
    arr.numberOfOccurrences(2) === 2;
    arr.numberOfOccurrences("a") === 0;

---
+++ 7th kyu/Number_Of_Occurrences.js
@@ -0,0 +1,17 @@
+/*
+DESCRIPTION:
+Write a function that returns the number of occurrences of an element in an array.
+
+Examples
+var arr = [0, 1, 2, 2, 3];
+arr.numberOfOccurrences(0) === 1;
+arr.numberOfOccurrences(4) === 0;
+arr.numberOfOccurrences(2) === 2;
+arr.numberOfOccurrences("a") === 0;
+*/
+
+//Solution    
+Array.prototype.numberOfOccurrences = function (n) {
+    //return the length of an array with only nth values
+    return this.filter(x => x == n).length
+}
