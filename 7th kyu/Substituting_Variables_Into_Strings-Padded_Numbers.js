ee9c36de81fc660631dd3504c94e45234a454b9a
David Iradukunda <iradukuda10@gmail.com>
2022-06-15 11:18:19 -0500

    Substituting_Variables_Into_Strings-Padded_Numbers
    Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
    
    Example:
    
    solution(5) // should return "Value is 00005"

---
+++ 7th kyu/Substituting_Variables_Into_Strings-Padded_Numbers.js
@@ -0,0 +1,14 @@
+/*
+DESCRIPTION:
+Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+Example:
+
+solution(5) // should return "Value is 00005"
+*/
+
+//Solution    
+function solution(value) {
+    //return the padded value using padStart               
+    return `Value is ${`${value}`.padStart(5, '0')}`
+}
