96877ff50ba276a30560c591a83342a6fa05388d
David Iradukunda <iradukuda10@gmail.com>
2022-06-27 18:41:33 -0500

    Largest_Square_Inside_A_Circle
    Determine the area of the largest square that can fit inside a circle with radius r.

---
+++ 7th kyu/Largest_Square_Inside_A_Circle.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+Determine the area of the largest square that can fit inside a circle with radius r.
+*/
+
+//Solution    
+function areaLargestSquare(r) {
+    //just math
+    return (r ** 2) * 2
+}
