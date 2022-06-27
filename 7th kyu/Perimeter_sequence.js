bb7fa6b9ed1e3b585179e286589e42ef77068c9a
David Iradukunda <iradukuda10@gmail.com>
2022-06-27 18:41:21 -0500

    Perimeter_sequence
    The first three stages of a sequence are shown.
    
    blocks
    
    The blocksize is a by a and a ≥ 1.
    
    What is the perimeter of the nth shape in the sequence (n ≥ 1) ?

---
+++ 7th kyu/Perimeter_sequence.js
@@ -0,0 +1,17 @@
+/*
+DESCRIPTION:
+Perimeter sequence
+The first three stages of a sequence are shown.
+
+blocks
+
+The blocksize is a by a and a ≥ 1.
+
+What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+*/
+
+//Solution    
+function perimeterSequence(a, n) {
+    //quick maths
+    return a * n * 4
+}
