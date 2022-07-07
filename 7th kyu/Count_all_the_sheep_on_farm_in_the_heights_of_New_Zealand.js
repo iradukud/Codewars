084b439e1ee567ec49675b1fe921a138eed5be61
David Iradukunda <iradukuda10@gmail.com>
2022-07-07 08:24:55 -0500

    Count_all_the_sheep_on_farm_in_the_heights_of_New_Zealand
    Every Friday and Saturday night, farmer counts amount of sheep returned back to his farm (sheep returned on Friday stay and don't leave for a weekend).
    
    Sheep return in groups each of the days -> you will be given two arrays with these numbers (one for Friday and one for Saturday night). Entries are always positive ints, higher than zero.
    
    Farmer knows the total amount of sheep, this is a third parameter. You need to return the amount of sheep lost (not returned to the farm) after final sheep counting on Saturday.
    
    Example 1: Input: {1, 2}, {3, 4}, 15 --> Output: 5
    
    Example 2: Input: {3, 1, 2}, {4, 5}, 21 --> Output: 6

---
+++ 7th kyu/Count_all_the_sheep_on_farm_in_the_heights_of_New_Zealand.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+Every Friday and Saturday night, farmer counts amount of sheep returned back to his farm (sheep returned on Friday stay and don't leave for a weekend).
+
+Sheep return in groups each of the days -> you will be given two arrays with these numbers (one for Friday and one for Saturday night). Entries are always positive ints, higher than zero.
+
+Farmer knows the total amount of sheep, this is a third parameter. You need to return the amount of sheep lost (not returned to the farm) after final sheep counting on Saturday.
+
+Example 1: Input: {1, 2}, {3, 4}, 15 --> Output: 5
+
+Example 2: Input: {3, 1, 2}, {4, 5}, 21 --> Output: 6
+
+Good luck! :-)
+*/
+
+//Solution    
+function lostSheep(friday, saturday, total) {
+    //variable that holds both days(arrays) as one array 
+    const returnedSheep = friday.concat(saturday)
+    //returned the amount of missing sheeps
+    return returnedSheep.length == 0 ? total : total - returnedSheep.reduce((sum, num) => sum + num)
+}
