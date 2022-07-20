88fdb6e9373f09fc4a5b880f49b40be18e1e42b6
David Iradukunda <iradukuda10@gmail.com>
2022-07-20 04:38:48 -0500

    The_Office_IV-Find_a_Meeting_Room
    Your job at E-Corp is both boring and difficult. It isn't made any easier by the fact that everyone constantly wants to have a meeting with you, and that the meeting rooms are always taken!
    
    In this kata, you will be given an array. Each value represents a meeting room. Your job? Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
    
    'X' --> busy
    'O' --> empty
    If all rooms are busy, return "None available!"

---
+++ 7th kyu/The_Office_IV-Find_a_Meeting_Room.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Your job at E-Corp is both boring and difficult. It isn't made any easier by the fact that everyone constantly wants to have a meeting with you, and that the meeting rooms are always taken!
+
+In this kata, you will be given an array. Each value represents a meeting room. Your job? Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
+
+'X' --> busy
+'O' --> empty
+If all rooms are busy, return "None available!"
+*/
+
+//Solution    
+function meeting(x) {
+    //variable that holds the position of the empty office
+    const openOffice = x.indexOf('O')
+    //returned the position of the empty office or a corresponding string message
+    return openOffice == -1 ? 'None available!' : openOffice
+}
