5c704e3b3ba93b6143204037819d8d6355091f92
David Iradukunda <iradukuda10@gmail.com>
2022-07-20 04:39:00 -0500

    Simple_Fun_#2-Circle_of_Numbers
    Task
    Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighbouring numbers is equal (note that 0 and n - 1 are neighbouring, too).
    
    Given n and firstNumber/first_number/first-number, find the number which is written in the radially opposite position to firstNumber.
    
    Example
    For n = 10 and firstNumber = 2, the output should be 7
    
    Input/Output
    [input] integer n
    
    A positive even integer.
    
    Constraints: 4 ≤ n ≤ 1000.
    
    [input] integer firstNumber/first_number/first-number
    
    Constraints: 0 ≤ firstNumber ≤ n - 1
    
    [output] an integer

---
+++ 7th kyu/Simple_Fun_#2-Circle_of_Numbers.js
@@ -0,0 +1,29 @@
+/*
+DESCRIPTION:
+Task
+Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighbouring numbers is equal (note that 0 and n - 1 are neighbouring, too).
+
+Given n and firstNumber/first_number/first-number, find the number which is written in the radially opposite position to firstNumber.
+
+Example
+For n = 10 and firstNumber = 2, the output should be 7
+
+Input/Output
+[input] integer n
+
+A positive even integer.
+
+Constraints: 4 ≤ n ≤ 1000.
+
+[input] integer firstNumber/first_number/first-number
+
+Constraints: 0 ≤ firstNumber ≤ n - 1
+
+[output] an integer
+*/
+
+//Solution    
+function circleOfNumbers(n, firstNumber) {
+    //returned the that is radially opposite position to firstNumber
+    return (firstNumber + n / 2) % n
+}
