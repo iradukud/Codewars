88d6d5b49bd9c4b4583f3fd698e142f1b0c58210
David Iradukunda <iradukuda10@gmail.com>
2022-07-22 21:56:16 -0500

    Sort_Santa's_Reindeer
    Happy Holidays fellow Code Warriors!
    Now, Dasher! Now, Dancer! Now, Prancer, and Vixen! On, Comet! On, Cupid! On, Donder and Blitzen! That's the order Santa wanted his reindeer...right? What do you mean he wants them in order by their last names!? Looks like we need your help Code Warrior!
    
    Sort Santa's Reindeer
    Write a function that accepts a sequence of Reindeer names, and returns a sequence with the Reindeer names sorted by their last names.
    
    Notes:
    It's guaranteed that each string is composed of two words
    In case of two identical last names, keep the original order
    Examples
    For this input:
    
    [
      "Dasher Tonoyan",
      "Dancer Moore",
      "Prancer Chua",
      "Vixen Hall",
      "Comet Karavani",
      "Cupid Foroutan",
      "Donder Jonker",
      "Blitzen Claus"
    ]
    You should return this output:
    
    [
      "Prancer Chua",
      "Blitzen Claus",
      "Cupid Foroutan",
      "Vixen Hall",
      "Donder Jonker",
      "Comet Karavani",
      "Dancer Moore",
      "Dasher Tonoyan",

---
+++ 7th kyu/Sort_Santa's_Reindeer.js
@@ -0,0 +1,50 @@
+/*
+DESCRIPTION:
+Happy Holidays fellow Code Warriors!
+Now, Dasher! Now, Dancer! Now, Prancer, and Vixen! On, Comet! On, Cupid! On, Donder and Blitzen! That's the order Santa wanted his reindeer...right? What do you mean he wants them in order by their last names!? Looks like we need your help Code Warrior!
+
+Sort Santa's Reindeer
+Write a function that accepts a sequence of Reindeer names, and returns a sequence with the Reindeer names sorted by their last names.
+
+Notes:
+It's guaranteed that each string is composed of two words
+In case of two identical last names, keep the original order
+Examples
+For this input:
+
+[
+  "Dasher Tonoyan", 
+  "Dancer Moore", 
+  "Prancer Chua", 
+  "Vixen Hall", 
+  "Comet Karavani",        
+  "Cupid Foroutan", 
+  "Donder Jonker", 
+  "Blitzen Claus"
+]
+You should return this output:
+
+[
+  "Prancer Chua",
+  "Blitzen Claus",
+  "Cupid Foroutan", 
+  "Vixen Hall", 
+  "Donder Jonker", 
+  "Comet Karavani",
+  "Dancer Moore", 
+  "Dasher Tonoyan",
+]
+*/
+
+//Solution    
+function sortReindeer(reindeerNames) {
+    return [...reindeerNames].sort((a, b) => {
+        const [, surname1] = a.split(" ")
+        const [, surname2] = b.split(" ")
+
+        if (surname1 > surname2) return 1
+        if (surname2 > surname1) return -1
+        return 0
+
+    })
+}
