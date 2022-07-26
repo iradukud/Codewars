9c17a6a682c18a26054f42c5e78dc877bb543f53
David Iradukunda <iradukuda10@gmail.com>
2022-07-25 22:14:04 -0500

    Thinking_&_Testing-A_and_B
    No Story
    
    No Description
    
    Only by Thinking and Testing
    
    Look at result of testcase, guess the code!

---
+++ 7th kyu/Thinking_&_Testing-A_and_B.js
@@ -0,0 +1,15 @@
+/*
+DESCRIPTION:
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+*/
+
+//Solution    
+function testit(a, b) {
+    return a | b
+}
