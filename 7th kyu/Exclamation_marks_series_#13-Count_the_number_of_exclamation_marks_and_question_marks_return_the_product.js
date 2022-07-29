e3bf2a4f8ebed16ac4c1e271e309fa2a0ad1aea6
David Iradukunda <iradukuda10@gmail.com>
2022-07-28 20:24:53 -0500

    Exclamation_marks_series_#13-Count_the_number_of_exclamation_marks_and_question_marks_return_the_product
    Count the number of exclamation marks and question marks, return the product.
    
    Examples
    Product("") == 0
    product("!") == 0
    Product("!ab? ?") == 2
    Product("!!") == 0
    Product("!??") == 2
    Product("!???") == 3
    Product("!!!??") == 6
    Product("!!!???") == 9
    Product("!???!!") == 9
    Product("!????!!!?") == 20

---
+++ 7th kyu/Exclamation_marks_series_#13-Count_the_number_of_exclamation_marks_and_question_marks_return_the_product.js
@@ -0,0 +1,23 @@
+/*
+DESCRIPTION:
+Count the number of exclamation marks and question marks, return the product.
+
+Examples
+Product("") == 0
+product("!") == 0
+Product("!ab? ?") == 2
+Product("!!") == 0
+Product("!??") == 2
+Product("!???") == 3
+Product("!!!??") == 6
+Product("!!!???") == 9
+Product("!???!!") == 9
+Product("!????!!!?") == 20
+
+*/
+
+//Solution    
+function product(string) {
+    //return the product of the frequency of ! and ?
+    return [...string].filter(x => x == '!').length * [...string].filter(x => x == '?').length
+}
