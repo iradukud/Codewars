1da9c4f25a7f52d90d521dfb42a7c5fa7a3accfa
David Iradukunda <iradukuda10@gmail.com>
2022-07-30 20:27:37 -0500

    Return_String_of_First_Characters
    In this exercise, a string is passed to a method and a new string has to be returned with the first character of each word in the string.
    
    For example:
    
    "This Is A Test" ==> "TIAT"
    Strings will only contain letters and spaces, with exactly 1 space between words, and no leading/trailing spaces.

---
+++ 7th kyu/Return_String_of_First_Characters.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+In this exercise, a string is passed to a method and a new string has to be returned with the first character of each word in the string.
+
+For example:
+
+"This Is A Test" ==> "TIAT"
+Strings will only contain letters and spaces, with exactly 1 space between words, and no leading/trailing spaces.
+
+*/
+
+//Solution    
+function makeString(s) {
+    //returned a string ith just the first character of each word in s
+    return s.split(' ').map(x => x[0]).join('')
+}
