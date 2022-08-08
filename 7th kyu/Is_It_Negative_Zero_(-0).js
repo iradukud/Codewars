1bf07f3bef64ce045db5781ee294f594e8235e3c
David Iradukunda <iradukuda10@gmail.com>
2022-08-08 14:28:06 -0500

    Is_It_Negative_Zero_(-0)
    There exist two zeroes: +0 (or just 0) and -0.
    
    Write a function that returns true if the input number is -0 and false otherwise (True and False for Python).
    
    In JavaScript / TypeScript / Coffeescript the input will be a number.
    
    In Python / Java / C / NASM / Haskell / the input will be a float.

---
+++ 7th kyu/Is_It_Negative_Zero_(-0).js
@@ -0,0 +1,17 @@
+/*
+DESCRIPTION:
+There exist two zeroes: +0 (or just 0) and -0.
+
+Write a function that returns true if the input number is -0 and false otherwise (True and False for Python).
+
+In JavaScript / TypeScript / Coffeescript the input will be a number.
+
+In Python / Java / C / NASM / Haskell / the input will be a float.
+
+*/
+
+//Solution    
+function isNegativeZero(n) {
+    //returned a boolean value based on whether the given number was negative zero
+    return n == 0 && 1 / n === -Infinity
+}
