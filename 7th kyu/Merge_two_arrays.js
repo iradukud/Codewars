c95778c47e052e58dcef70ebbcd83d28fd523923
David Iradukunda <iradukuda10@gmail.com>
2022-08-08 14:28:39 -0500

    Merge_two_arrays
    Write a function that combines two arrays by alternatingly taking elements from each array in turn.
    
    Examples:
    
    [a, b, c, d, e], [1, 2, 3, 4, 5] becomes  [a, 1, b, 2, c, 3, d, 4, e, 5]
    
    [1, 2, 3], [a, b, c, d, e, f] becomes [1, a, 2, b, 3, c, d, e, f]
    Points:
    
    The arrays may be of different lengths, with at least one character/digit.
    One array will be of string characters (in lower case, a-z), a second of integers (all positive starting at 1).

---
+++ 7th kyu/Merge_two_arrays.js
@@ -0,0 +1,31 @@
+/*
+DESCRIPTION:
+Write a function that combines two arrays by alternatingly taking elements from each array in turn.
+
+Examples:
+
+[a, b, c, d, e], [1, 2, 3, 4, 5] becomes  [a, 1, b, 2, c, 3, d, 4, e, 5]
+
+[1, 2, 3], [a, b, c, d, e, f] becomes [1, a, 2, b, 3, c, d, e, f]
+Points:
+
+The arrays may be of different lengths, with at least one character/digit.
+One array will be of string characters (in lower case, a-z), a second of integers (all positive starting at 1).
+Inspired by https://adriann.github.io/programming_problems.html
+
+*/
+
+//Solution    
+function mergeArrays(a, b) {
+    //variable that holds the length of the longest array
+    const max = Math.max(a.length, b.length)
+    //variable that will hold the merged arrays
+    const arr = []
+    //loop the runs the length of the longest array and push/places each item into the new array 
+    for (i = 0; i < max; i++) {
+        if (a[i]) { arr.push(a[i]) }
+        if (b[i]) { arr.push(b[i]) }
+    }
+    //returned the merged arr   
+    return arr
+}
