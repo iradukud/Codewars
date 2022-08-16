b367d860c4065ee169b8c7e088433e8163e24177
David Iradukunda <iradukuda10@gmail.com>
2022-08-16 17:50:45 -0500

    Words_to_sentence
    Write function which will create a string from a list of strings, separated by space.

---
+++ 7th kyu/Words_to_sentence.js
@@ -0,0 +1,15 @@
+/*
+Write function which will create a string from a list of strings, separated by space.
+
+Example:
+
+["hello", "world"] -> "hello world"
+
+
+*/
+
+//Solution    
+function wordsToSentence(words) {
+    //returned the array of words back as a sentence
+    return words.join(' ')
+}
