a0fa8786bd2a3eb8465985a476bd78c7a30370ae
David Iradukunda <iradukuda10@gmail.com>
2022-08-16 17:51:22 -0500

    Dictionary_from_two_lists
    There are two lists, possibly of different lengths. The first one consists of keys, the second one consists of values. Write a function createDict(keys, values) that returns a dictionary created from keys and values. If there are not enough values, the rest of keys should have a None (JS null)value. If there not enough keys, just ignore the rest of values.
    
    Example 1:
    
    keys = ['a', 'b', 'c', 'd']
    values = [1, 2, 3]
    createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3, 'd': null}
    Example 2:
    
    keys = ['a', 'b', 'c']
    values = [1, 2, 3, 4]
    createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3}

---
+++ 7th kyu/Dictionary_from_two_lists.js
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+There are two lists, possibly of different lengths. The first one consists of keys, the second one consists of values. Write a function createDict(keys, values) that returns a dictionary created from keys and values. If there are not enough values, the rest of keys should have a None (JS null)value. If there not enough keys, just ignore the rest of values.
+
+Example 1:
+
+keys = ['a', 'b', 'c', 'd']
+values = [1, 2, 3]
+createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3, 'd': null}
+Example 2:
+
+keys = ['a', 'b', 'c']
+values = [1, 2, 3, 4]
+createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3}
+
+*/
+
+//Solution    
+function createDict(keys, values) {
+    //variable that holds the object that will be returned
+    const obj = {}
+    //loop that goes through all the keys and places the key value pair into the obj
+    keys.forEach((x, i) => obj[x] = values[i] == '' || values[i] || values[i] == false ? values[i] : null)
+    //returned the final obj             
+    return obj
+}
