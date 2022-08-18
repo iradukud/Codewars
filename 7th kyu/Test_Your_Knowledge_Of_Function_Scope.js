f2ea97104ee177ccc29100e80f3923b8082360d6
David Iradukunda <iradukuda10@gmail.com>
2022-08-18 10:49:44 -0500

    Test_Your_Knowledge_Of_Function_Scope
    Write a function that adds from two invocations.
    
    All inputs will be integers.
    
    add(3)(4)  // 7
    add(12)(20) // 32

---
+++ 7th kyu/Test_Your_Knowledge_Of_Function_Scope.js
@@ -0,0 +1,13 @@
+/*
+DESCRIPTION:
+Write a function that adds from two invocations.
+
+All inputs will be integers.
+
+add(3)(4)  // 7
+add(12)(20) // 32
+
+*/
+
+//Solution    
+const add = numberA => numberB => numberA + numberB
