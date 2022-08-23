d9618c6b616399fc2a75944561f6fbed4b140451
David Iradukunda <iradukuda10@gmail.com>
2022-08-23 00:56:56 -0500

    99_Bottles_of_Beer
    Instructions
    Complete the function that returns the lyrics for the song 99 Bottles of Beer as an array of strings: each line should be a separate element - see the example at the bottom.
    
    Note: in order to avoid hardcoded solutions, the size of your code is limited to 1000 characters
    
    Lyrics
    99 bottles of beer on the wall, 99 bottles of beer.
    Take one down and pass it around, 98 bottles of beer on the wall.
    
    98 bottles of beer on the wall, 98 bottles of beer.
    Take one down and pass it around, 97 bottles of beer on the wall.
    
    ...and so on...
    
    3 bottles of beer on the wall, 3 bottles of beer.
    Take one down and pass it around, 2 bottles of beer on the wall.
    
    2 bottles of beer on the wall, 2 bottles of beer.
    Take one down and pass it around, 1 bottle of beer on the wall.
    
    1 bottle of beer on the wall, 1 bottle of beer.
    Take one down and pass it around, no more bottles of beer on the wall.
    
    No more bottles of beer on the wall, no more bottles of beer.
    Go to the store and buy some more, 99 bottles of beer on the wall.
    
    Example
    [ "99 bottles of beer on the wall, 99 bottles of beer.",
      "Take one down and pass it around, 98 bottles of beer on the wall.",
      "98 bottles of beer on the wall, 98 bottles of beer.",
    
      ...and so on...
    
      "3 bottles of beer on the wall, 3 bottles of beer.",
      "Take one down and pass it around, 2 bottles of beer on the wall.",
      "2 bottles of beer on the wall, 2 bottles of beer.",
      "Take one down and pass it around, 1 bottle of beer on the wall.",
      "1 bottle of beer on the wall, 1 bottle of beer.",
      "Take one down and pass it around, no more bottles of beer on the wall.",
      "No more bottles of beer on the wall, no more bottles of beer.",
      "Go to the store and buy some more, 99 bottles of beer on the wall." ]

---
+++ 7th kyu/99_Bottles_of_Beer.js
@@ -0,0 +1,65 @@
+/*
+DESCRIPTION:
+Instructions
+Complete the function that returns the lyrics for the song 99 Bottles of Beer as an array of strings: each line should be a separate element - see the example at the bottom.
+
+Note: in order to avoid hardcoded solutions, the size of your code is limited to 1000 characters
+
+Lyrics
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+
+98 bottles of beer on the wall, 98 bottles of beer.
+Take one down and pass it around, 97 bottles of beer on the wall.
+
+...and so on...
+
+3 bottles of beer on the wall, 3 bottles of beer.
+Take one down and pass it around, 2 bottles of beer on the wall.
+
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+
+Example
+[ "99 bottles of beer on the wall, 99 bottles of beer.",
+  "Take one down and pass it around, 98 bottles of beer on the wall.",
+  "98 bottles of beer on the wall, 98 bottles of beer.",
+
+  ...and so on...
+
+  "3 bottles of beer on the wall, 3 bottles of beer.",
+  "Take one down and pass it around, 2 bottles of beer on the wall.",
+  "2 bottles of beer on the wall, 2 bottles of beer.",
+  "Take one down and pass it around, 1 bottle of beer on the wall.",
+  "1 bottle of beer on the wall, 1 bottle of beer.",
+  "Take one down and pass it around, no more bottles of beer on the wall.",
+  "No more bottles of beer on the wall, no more bottles of beer.",
+  "Go to the store and buy some more, 99 bottles of beer on the wall." ]
+
+*/
+
+//Solution    
+var sing = function () {
+    // variable that will hold the array of the lyrics
+    const lyrics = []
+    //loop that places each line of lyric into the array till the 2nd last one
+    for (i = 99; i >= 3; i--) {
+        lyrics.push(i + ' bottles of beer on the wall, ' + i + ' bottles of beer.')
+        lyrics.push('Take one down and pass it around, ' + (i - 1) + ' bottles of beer on the wall.')
+    }
+    //returned the final lyrics + the last few lines
+    return lyrics.concat([
+        '2 bottles of beer on the wall, 2 bottles of beer.',
+        'Take one down and pass it around, 1 bottle of beer on the wall.',
+        '1 bottle of beer on the wall, 1 bottle of beer.',
+        'Take one down and pass it around, no more bottles of beer on the wall.',
+        'No more bottles of beer on the wall, no more bottles of beer.',
+        'Go to the store and buy some more, 99 bottles of beer on the wall.'
+    ])
+}
