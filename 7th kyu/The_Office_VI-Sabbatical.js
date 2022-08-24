fc68ca235278dc3b2980de5f10e1d99488c7e3df
David Iradukunda <iradukuda10@gmail.com>
2022-08-24 17:48:39 -0500

    The_Office_VI-Sabbatical
    Learning to code around your full time job is taking over your life. You realise that in order to make significant steps quickly, it would help to go to a coding bootcamp in London.
    
    Problem is, many of them cost a fortune, and those that don't still involve a significant amount of time off work - who will pay your mortgage?!
    
    To offset this risk, you decide that rather than leaving work totally, you will request a sabbatical so that you can go back to work post bootcamp and be paid while you look for your next role.
    
    You need to approach your boss. Her decision will be based on three parameters:
    
    val = your value to the organisation
    happiness = her happiness level at the time of asking and finally
    The numbers of letters from 'sabbatical' that are present in string s.
    
    Note that if s contains three instances of the letter 'l', that still scores three points, even though there is only one in the word sabbatical.
    
    If the sum of the three parameters (as described above) is > 22, return 'Sabbatical! Boom!', else return 'Back to your desk, boy.'.

---
+++ 7th kyu/The_Office_VI-Sabbatical.js
@@ -0,0 +1,27 @@
+/*
+DESCRIPTION:
+Learning to code around your full time job is taking over your life. You realise that in order to make significant steps quickly, it would help to go to a coding bootcamp in London.
+
+Problem is, many of them cost a fortune, and those that don't still involve a significant amount of time off work - who will pay your mortgage?!
+
+To offset this risk, you decide that rather than leaving work totally, you will request a sabbatical so that you can go back to work post bootcamp and be paid while you look for your next role.
+
+You need to approach your boss. Her decision will be based on three parameters:
+
+val = your value to the organisation
+happiness = her happiness level at the time of asking and finally
+The numbers of letters from 'sabbatical' that are present in string s.
+
+Note that if s contains three instances of the letter 'l', that still scores three points, even though there is only one in the word sabbatical.
+
+If the sum of the three parameters (as described above) is > 22, return 'Sabbatical! Boom!', else return 'Back to your desk, boy.'.
+
+*/
+
+//Solution    
+function sabb(s, val, happiness) {
+    //make s equal the amount/length of characters in s that are in the word Sabbatical
+    s = (s.match(/[sabticl]/gi) || []).length
+    //returned the corresponding string depending on whether the given parameters were sufficent
+    return s + val + happiness > 22 ? 'Sabbatical! Boom!' : 'Back to your desk, boy.'
+}
