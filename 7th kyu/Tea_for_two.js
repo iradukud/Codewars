cc8a89738c7a5e0504d5bb7901af33ffa77a14bb
iradukud <iradukuda10@gmail.com>
2022-08-29 10:53:15 -0500

    Tea_for_two
    Given a string or number write a function tea42 that takes as input a string or number and returns a string where every '2' charcter or digit has been replaced with a t.
    
    tea42('coffee') //should return 'coffee'
    tea42('tea') //should return 'tea'
    tea42('2ea') //should return 'tea'
    tea42(9022) //should return '90tt'
    tea42(5676765) //should return '5676765'
    tea42('2u2u') //should return 'tutu'

---
+++ 7th kyu/Tea_for_two.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Given a string or number write a function tea42 that takes as input a string or number and returns a string where every '2' charcter or digit has been replaced with a t.
+
+tea42('coffee') //should return 'coffee'
+tea42('tea') //should return 'tea'
+tea42('2ea') //should return 'tea'
+tea42(9022) //should return '90tt'
+tea42(5676765) //should return '5676765'
+tea42('2u2u') //should return 'tutu'
+
+*/
+
+//Solution  
+function tea42(input) {
+    //returned the input with all the 2 replaced by t's
+    return (input + '').replace(/[2]/g, 't')
+}
