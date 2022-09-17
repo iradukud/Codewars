b6f27a2d213cf93db2f4824a68e4c71bae1d996e
iradukud <iradukuda10@gmail.com>
2022-09-16 19:24:39 -0500

    Basics_04-Rotate_Matrix
    Your task is to rotate a matrix 90 degree to the left. The matrix is an array of integers with dimension n,m. So this kata checks some basics, it's not too difficult.
    
    There's nothing more to explain, no tricks, no "bad cases";-). Perhaps you take a look at the testcases...
    
    One easy example:
    
    Input: {{-1, 4, 5},
            { 2, 3, 4}}
    
    Output: {{ 5, 4},
             { 4, 3},
             {-1, 2}}
    First there are some static tests, later on random tests too...
    
    Hope you have fun:-)!

---
+++ 7th kyu/Basics_04-Rotate_Matrix.js
@@ -0,0 +1,33 @@
+/*
+DESCRIPTION:
+Your task is to rotate a matrix 90 degree to the left. The matrix is an array of integers with dimension n,m. So this kata checks some basics, it's not too difficult.
+
+There's nothing more to explain, no tricks, no "bad cases";-). Perhaps you take a look at the testcases...
+
+One easy example:
+
+Input: {{-1, 4, 5},
+        { 2, 3, 4}}
+
+Output: {{ 5, 4},
+         { 4, 3},
+         {-1, 2}}
+First there are some static tests, later on random tests too...
+
+
+Hope you have fun:-)!
+
+*/
+
+//Solution  
+var rotateMatrix = arr => {
+    let rotatedArr = []
+    for(let i=0; i<arr[0].length; i++){
+      let subArr = []
+      for(let j=0; j<arr.length; j++){
+        subArr.push(arr[j][i])
+      }
+      rotatedArr.push(subArr)
+    }
+    return rotatedArr.reverse()
+  }
