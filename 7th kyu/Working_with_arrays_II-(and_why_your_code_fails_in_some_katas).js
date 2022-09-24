4bd3e0ff9ba63a1c5770cf6a9af9477b525ece6b
iradukud <iradukuda10@gmail.com>
2022-09-24 01:15:21 -0500

    Working_with_arrays_II-(and_why_your_code_fails_in_some_katas)
    In this kata the function returns an array/list like the one passed to it but with its nth element removed (with 0 <= n <= array/list.length - 1). The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
    
    Good luck!
    
    Some good reading: MDN Docs about arrays

---
+++ 7th kyu/Working_with_arrays_II-(and_why_your_code_fails_in_some_katas).js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+In this kata the function returns an array/list like the one passed to it but with its nth element removed (with 0 <= n <= array/list.length - 1). The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+Good luck!
+
+Some good reading: MDN Docs about arrays
+
+*/
+
+//Solution  
+function removeNthElement(arr, n) {
+    var arrCopy = [...arr]
+    arrCopy.splice(n, 1) // removes the nth element
+    return arrCopy
+}
