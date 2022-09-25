6470308623ec672480a5f3c98f34aaa43c653454
iradukud <iradukuda10@gmail.com>
2022-09-25 12:43:57 -0500

    Multiply_the_strings_in_the_array
    You received an array with two strings. Create a function that will return their product as a string. E.g.
    
    arrMultiply(['9','6']) should return '54'

---
+++ 7th kyu/Multiply_the_strings_in_the_array.js
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+You received an array with two strings. Create a function that will return their product as a string. E.g.
+
+arrMultiply(['9','6']) should return '54'
+
+*/
+
+//Solution  
+function arrMultiply(arr) {
+    return (arr[0] * arr[1]) + ''
+}
