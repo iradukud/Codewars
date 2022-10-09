34da23e0d142808d38ef40e5e7f45a7e4668159c
iradukud <iradukuda10@gmail.com>
2022-10-09 16:09:40 -0500

    String_Matcher
    isMatching checks if a string can be created by combining and rearranging the letters of two other strings (not case sensitive).
    
    !Spaces will be ignored but special characters and numbers won't match the string and return false.
    
    For example:
    isMatching("email box", "b aIl", "Mo x e") return true
    but
    isMatching("bouh", "0b", "uh") return false
    
    You need to be able to use all the caracters from the two strings so:
    isMatching("kata", "kt", "aaa") return false

---
+++ 7th kyu/String_Matcher.js
@@ -0,0 +1,21 @@
+/*
+DESCRIPTION:
+isMatching checks if a string can be created by combining and rearranging the letters of two other strings (not case sensitive).
+
+!Spaces will be ignored but special characters and numbers won't match the string and return false.
+
+For example:
+isMatching("email box", "b aIl", "Mo x e") return true
+but
+isMatching("bouh", "0b", "uh") return false
+
+You need to be able to use all the caracters from the two strings so:
+isMatching("kata", "kt", "aaa") return false
+
+*/
+
+//Solution  
+function isMatching(string, str1, str2) {
+    return ([...string].sort((a, b) => a.localeCompare(b)).join``).toLowerCase().trim() ===
+        ([...(str1 + str2)].sort((a, b) => a.localeCompare(b)).join``).toLowerCase().trim()
+}
