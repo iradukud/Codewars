5bd80ecd5e20974ffac7cc46bd498d21ab9a1a39
iradukud <iradukuda10@gmail.com>
2022-10-17 13:59:46 -0500

    [Geometry_A-2]-Length_of_a_vector
    For a given 2D vector described by cartesian coordinates of its initial point and terminal point in the following format:
    
    [[x1, y1], [x2, y2]]
    Your function must return the vector's length represented as a floating point number.
    Error must be within 1e-7.
    
    Coordinates can be integers or floating point numbers.

---
+++ 7th kyu/[Geometry_A-2]-Length_of_a_vector.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+For a given 2D vector described by cartesian coordinates of its initial point and terminal point in the following format:
+
+[[x1, y1], [x2, y2]]
+Your function must return the vector's length represented as a floating point number.
+Error must be within 1e-7.
+
+Coordinates can be integers or floating point numbers.
+
+*/
+
+//Solution  
+function vectorLength([[x1, y1], [x2, y2]]) {
+    return Math.hypot((x2 - x1), (y2 - y1))
+}
