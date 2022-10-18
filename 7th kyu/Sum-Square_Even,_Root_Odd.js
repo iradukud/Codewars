a1fd0f6101b76f39e6ce7d760f7bbef410c3f507
iradukud <iradukuda10@gmail.com>
2022-10-18 01:32:00 -0500

    Sum-Square_Even,_Root_Odd
    Complete the function that takes a list of numbers (nums), as the only argument to the function. Take each number in the list and square it if it is even, or square root the number if it is odd. Take this new list and return the sum of it, rounded to two decimal places.
    
    The list will never be empty and will only contain values that are greater than or equal to zero.
    
    Good luck!

---
+++ 7th kyu/Sum-Square_Even,_Root_Odd.js
@@ -0,0 +1,14 @@
+/*
+DESCRIPTION:
+Complete the function that takes a list of numbers (nums), as the only argument to the function. Take each number in the list and square it if it is even, or square root the number if it is odd. Take this new list and return the sum of it, rounded to two decimal places.
+
+The list will never be empty and will only contain values that are greater than or equal to zero.
+
+Good luck!
+
+*/
+
+//Solution  
+const sumSquareEvenRootOdd = ns => {
+    return +ns.reduce((sum, num) => num % 2 ? Math.sqrt(num) + sum : num ** 2 + sum, 0).toFixed(2)
+}
