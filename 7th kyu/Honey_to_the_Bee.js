851193478c7137cec9b3f176fe790d5ac83c8cca
iradukud <iradukuda10@gmail.com>
2022-10-24 19:22:20 -0500

    Honey_to_the_Bee
    Find out why the amount of honey in the hive is not increasing.

---
+++ 7th kyu/Honey_to_the_Bee.js
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+Find out why the amount of honey in the hive is not increasing.
+
+*/
+
+//Solution  
+function Bee(capacity, hive) {
+    this.capacity = capacity;
+    this.hive = hive;
+}
+
+function Hive() {
+    this.pollen = 100;
+}
+
+//TODO: The amount of honey in the hive is not increasing.
+Hive.prototype.addPollen = function (pollen) {
+    this.pollen += pollen;
+}
+
+Hive.prototype.getPollen = function () {
+    return this.pollen;
+}
+
+Bee.prototype.unloadPollen = function () {
+    this.hive.addPollen(this.capacity);
+}
