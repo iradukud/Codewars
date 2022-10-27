1180d1b74ecf45c134607744821da47873f14536
iradukud <iradukuda10@gmail.com>
2022-10-26 22:07:35 -0500

    Coding_3min-Bug_in_Apple
    This is the simple version of Shortest Code series. If you need some challenges, please try the challenge version.
    
    Task:
    Find out "B"(Bug) in a lot of "A"(Apple).
    
    There will always be one bug in apple, not need to consider the situation that without bug or more than one bugs.
    
    input: string Array apple
    
    output: Location of "B", [x,y]

---
+++ 7th kyu/Coding_3min-Bug_in_Apple.js
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+This is the simple version of Shortest Code series. If you need some challenges, please try the challenge version.
+
+Task:
+Find out "B"(Bug) in a lot of "A"(Apple).
+
+There will always be one bug in apple, not need to consider the situation that without bug or more than one bugs.
+
+input: string Array apple
+
+output: Location of "B", [x,y]
+
+*/
+
+//Solution  
+function sc(apple) {
+    for (i = 0; i < apple.length; i++)
+        for (j = 0; j < apple[i].length; j++) {
+            if (apple[i][j] == "B")
+                return [i, j];
+        }
+    return null;
+}
