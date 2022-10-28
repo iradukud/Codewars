f64e3c4af8635b18a2eb45e85c92e281678290c6
iradukud <iradukuda10@gmail.com>
2022-10-27 22:09:01 -0500

    Pairs_of_integers_from_m_to_n
    Implement a function that receives two integers m and n and generates a sorted list of pairs (a, b) such that m <= a <= b <= n.
    
    The input m will always be smaller than or equal to n (e.g., m <= n)
    
    Example
    m = 2
    n = 4
    
    result = [(2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (4, 4)]

---
+++ 7th kyu/Pairs_of_integers_from_m_to_n.js
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+Implement a function that receives two integers m and n and generates a sorted list of pairs (a, b) such that m <= a <= b <= n.
+
+The input m will always be smaller than or equal to n (e.g., m <= n)
+
+Example
+m = 2
+n = 4
+
+result = [(2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (4, 4)]
+
+*/
+
+//Solution  
+function generatePairs(m, n) {
+    const arr = []
+    for (j = m; j <= n; j++) {
+        for (i = j; i <= n; i++) {
+            arr.push([j, i])
+        }
+    }
+    return arr
+}
