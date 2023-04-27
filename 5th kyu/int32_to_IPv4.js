01e2256619374cef0321baa6d1fe40db548fe87a
iradukud <iradukuda10@gmail.com>
2023-04-27 16:32:00 -0500

    int32_to_IPv4
    Take the following IPv4 address: 128.32.10.1
    
    This address has 4 octets where each octet is a single byte (or 8 bits).
    
    1st octet 128 has the binary representation: 10000000
    2nd octet 32 has the binary representation: 00100000
    3rd octet 10 has the binary representation: 00001010
    4th octet 1 has the binary representation: 00000001
    So 128.32.10.1 == 10000000.00100000.00001010.00000001
    
    Because the above IP address has 32 bits, we can represent it as the unsigned 32 bit number: 2149583361
    
    Complete the function that takes an unsigned 32 bit number and returns a string representation of its IPv4 address.
    
    Examples
    2149583361 ==> "128.32.10.1"
    32         ==> "0.0.0.32"
    0          ==> "0.0.0.0"

---
+++ 5th kyu/int32_to_IPv4.js
@@ -0,0 +1,44 @@
+/*
+DESCRIPTION:
+Take the following IPv4 address: 128.32.10.1
+
+This address has 4 octets where each octet is a single byte (or 8 bits).
+
+1st octet 128 has the binary representation: 10000000
+2nd octet 32 has the binary representation: 00100000
+3rd octet 10 has the binary representation: 00001010
+4th octet 1 has the binary representation: 00000001
+So 128.32.10.1 == 10000000.00100000.00001010.00000001
+
+Because the above IP address has 32 bits, we can represent it as the unsigned 32 bit number: 2149583361
+
+Complete the function that takes an unsigned 32 bit number and returns a string representation of its IPv4 address.
+
+Examples
+2149583361 ==> "128.32.10.1"
+32         ==> "0.0.0.32"
+0          ==> "0.0.0.0"
+
+*/
+
+//Solution  
+function int32ToIp(int32) {
+    let binaryNumber = int32.toString(2).padStart(32, '0');
+    let buffer = [];
+    let result = [];
+    let i = 8;
+  
+    for (let index = 0; index < 4; index++) {
+      buffer[index] = binaryNumber.slice(i - 8, i);
+      i += 8;
+      if (!buffer[index]) {
+        buffer[index] = '0';
+      }
+    }
+  
+    for (let i = 0; i < buffer.length; i++) {
+      result[i] = parseInt(buffer[i], 2);
+    }
+  
+    return result.toString().replace(/,/g, '.');
+  }
