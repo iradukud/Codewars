792078c8fe5f9e3eee5e58ffa1b54b084d2750c1
iradukud <iradukuda10@gmail.com>
2023-06-03 21:09:26 -0500

    Rot13

--- 5th kyu/Rot13.js
+++ 5th kyu/Rot13.js
@@ -1,9 +1,18 @@
 /*
 DESCRIPTION:
-ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+How can you tell an extrovert from an introvert at NSA?
+Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
 
-Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it?
+According to Wikipedia, ROT13 is frequently used to obfuscate jokes on USENET.
 
+For this task you're only supposed to substitute characters. Not spaces, punctuation, numbers, etc.
+
+Test examples:
+
+"EBG13 rknzcyr." -> "ROT13 example."
+
+"This is my first ROT13 excercise!" -> "Guvf vf zl svefg EBG13 rkprepvfr!"
 */
 
 //Solution  
