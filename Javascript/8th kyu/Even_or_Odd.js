061ce018a394b9f140bb30e094d6700ec7db5f2a
iradukud <iradukuda10@gmail.com>
2023-06-12 18:03:32 -0500

    Even_or_Odd
    refactored

--- Javascript/8th kyu/Even_or_Odd.js
+++ Javascript/8th kyu/Even_or_Odd.js
@@ -4,11 +4,5 @@
 
 //answer
 function even_or_odd(number) {
-    //If a number is divisble by two it's even
-    if (number % 2 == 0) {
-        return 'Even'
-    //If a number is not divisble by two it's odd    
-    } else {
-        return 'Odd'
-    }
+    return number % 2 == 0 ? "Even" : "Odd"
 }
